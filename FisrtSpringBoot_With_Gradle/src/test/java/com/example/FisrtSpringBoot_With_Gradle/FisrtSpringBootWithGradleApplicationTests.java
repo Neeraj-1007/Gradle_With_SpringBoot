4bdcdd6b3cc3623d0f4784e8569f18f9e0b1119d
Neeraj-1007 <neerajkumarjha10@gmail.com>
2023-02-19 17:32:05 +0530

    Initial Commit

---
+++ FisrtSpringBoot_With_Gradle/src/test/java/com/example/FisrtSpringBoot_With_Gradle/FisrtSpringBootWithGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FisrtSpringBoot_With_Gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisrtSpringBootWithGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
